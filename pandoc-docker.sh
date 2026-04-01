902f63675b80506f0705e116d75e5698235fb5d4
minstrike520 <minstrike520@gmail.com>
2026-04-01 09:41:32 +0800

    feat: more image options

--- pandoc-docker.sh
+++ pandoc-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 常數
-
-DOCKER_IMAGE=pandoc-alpine:latest
+LOCAL_IMAGE="pandoc-docker:main"
+GHCR_IMAGE="ghcr.io/minstrike520/pandoc-docker:main"
 
 # 用法提示函式
 usage() {
@@ -10,6 +10,12 @@ usage() {
     echo "範例: $0 hw1.md header.tex -o final_report.pdf"
     exit 1
 }
+
+# 檢查 Docker 執行權限
+if ! docker info >/dev/null 2>&1; then
+    echo "錯誤: 無法連接 Docker daemon。請確認 Docker 已啟動且您有執行權限。"
+    exit 1
+fi
 
 # 檢查必填參數數量
 if [ "$#" -lt 2 ]; then
@@ -40,6 +46,27 @@ while getopts ":o:" opt; do
       ;;
   esac
 done
+
+# 決定要使用的 Docker 映像檔
+
+if [ -n "$PANDOC_DOCKER_IMAGE" ]; then
+    if ! docker image inspect "$PANDOC_DOCKER_IMAGE" >/dev/null 2>&1; then
+      echo "錯誤：找不到映像檔 '$PANDOC_DOCKER_IMAGE'"
+      exit 1
+    fi
+    DOCKER_IMAGE="$PANDOC_DOCKER_IMAGE"
+else
+    if docker image inspect "$LOCAL_IMAGE" >/dev/null 2>&1; then
+        DOCKER_IMAGE="$LOCAL_IMAGE"
+        echo "使用映像檔 '$LOCAL_IMAGE'"
+    elif docker image inspect "$GHCR_IMAGE" >/dev/null 2>&1; then
+        DOCKER_IMAGE="$GHCR_IMAGE"
+        echo "使用映像檔 '$GHCR_IMAGE'"
+    else
+        echo "錯誤：找不到映像檔 '$LOCAL_IMAGE' 或 '$GHCR_IMAGE'"
+        exit 1
+    fi
+fi
 
 # 檢查輸入檔案是否存在
 if [ ! -f "$INPUT_MD" ]; then
